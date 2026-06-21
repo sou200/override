73037bf153ac8072a6cf3573ec30b76774c1238f
Soufiane Erhouni <serhouni@e1r9p15.1337.ma>
2026-06-21 15:38:48 +0100

    all levels are done

---
+++ level08/source.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void log_wrapper(FILE *logfile, const char *message, const char *user_input)
+{
+    char buffer[264];
+
+    // Copy prefix message
+    strcpy(buffer, message);
+
+    // Append user input (VULNERABLE)
+    snprintf(buffer + strlen(buffer), sizeof(buffer) - strlen(buffer), user_input);
+
+    // Remove newline if present
+    buffer[strcspn(buffer, "\n")] = '\0';
+
+    fprintf(logfile, "LOG: %s\n", buffer);
+}
+
+int main(int argc, char *argv[])
+{
+    FILE *log_file;
+    FILE *source_file;
+
+    int backup_fd;
+    char c;
+    char backup_path[104];
+
+    if (argc != 2)
+    {
+        printf("Usage: %s filename\n", argv[0]);
+        return 1;
+    }
+
+    /* Open log */
+
+    log_file = fopen("./backups/.log", "w");
+
+    if (!log_file)
+    {
+        printf("ERROR: Failed to open %s\n", "./backups/.log");
+        exit(1);
+    }
+
+    log_wrapper(log_file, "Starting back up: ", argv[1]);
+
+    /* Open source file */
+
+    source_file = fopen(argv[1], "r");
+
+    if (!source_file)
+    {
+        printf("ERROR: Failed to open %s\n", argv[1]);
+        exit(1);
+    }
+
+    /* Build destination path */
+
+    strcpy(backup_path, "./backups/");
+
+    strncat(backup_path, argv[1], sizeof(backup_path) - strlen(backup_path) - 1);
+
+    /* Create backup file */
+
+    backup_fd = open(backup_path, O_WRONLY | O_CREAT | O_APPEND, 0660);
+
+    if (backup_fd < 0)
+    {
+        printf("ERROR: Failed to open %s%s\n", "./backups/", argv[1]
+        );
+
+        exit(1);
+    }
+
+    /* Copy file contents */
+
+    while ((c = fgetc(source_file)) != EOF)
+    {
+        write(backup_fd, &c, 1);
+    }
+
+    log_wrapper(log_file,"Finished back up ",argv[1]);
+
+    fclose(source_file);
+
+    fclose(log_file);
+
+    close(backup_fd);
+
+    return 0;
+}
---
+++ level09/source.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void secret_backdoor(void)
+{
+    char command[128];
+
+    fgets(command, sizeof(command), stdin);
+    system(command);
+}
+
+void set_username(char *data)
+{
+    char username[128];
+    int i;
+
+    memset(username, 0, sizeof(username));
+
+    puts(">: Enter your username");
+    printf(">>: ");
+
+    fgets(username, sizeof(username), stdin);
+
+    for (i = 0; i <= 40 && username[i] != '\0'; i++)
+    {
+        data[140 + i] = username[i];
+    }
+
+    printf(">: Welcome, %s", data + 140);
+}
+
+void set_msg(char *data)
+{
+    char message[1024];
+
+    memset(message, 0, sizeof(message));
+
+    puts(">: Msg @Unix-Dude");
+    printf(">>: ");
+
+    fgets(message, sizeof(message), stdin);
+
+    strncpy(data, message, *(int *)(data + 180));
+}
+
+void handle_msg(void)
+{
+    struct
+    {
+        char msg[140];
+        char username[40];
+        int msg_len;
+    } user_data;
+
+    memset(&user_data, 0, sizeof(user_data));
+
+    user_data.msg_len = 140;
+
+    set_username((char *)&user_data);
+    set_msg((char *)&user_data);
+
+    puts(">: Msg sent!");
+}
+
+int main(void)
+{
+    puts(
+        "--------------------------------------------\n"
+        "|   ~Welcome to l33t-m$n ~    v1337        |\n"
+        "--------------------------------------------"
+    );
+
+    handle_msg();
+
+    return 0;
+}
